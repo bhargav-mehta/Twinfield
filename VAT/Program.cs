2b6e8deabf6e47121b2271afb7d7f840aa0b50cf
Bhargav Mehta <bhargav.mehta@wolterskluwer.com>
2018-07-26 12:27:57 +0530

    VatServiceModified

--- VAT/Program.cs
+++ VAT/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Console = System.Console;
 
 namespace VAT
 {
@@ -11,6 +12,8 @@ namespace VAT
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome to VAT Service");
+			Console.WriteLine("This is commit 2 line");
+			Console.ReadLine();
 		}
 	}
 }
